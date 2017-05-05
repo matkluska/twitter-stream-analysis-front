90662b8137eaa7766551121ab95a4847d31bcd4b
Mateusz Nowak <mateusznowak@vp.pl>
2017-05-05 22:37:42 +0200

    Added basic interval

--- www/js/hashtags.js
+++ www/js/hashtags.js
@@ -1,17 +1,30 @@
+var prevHashtags = [];
+var prevNumberOfTweets = 10;
+
 $(function () {
     $('#submitButton').click(function () {
         var hashtags = $("#hashtags").tagsinput('items');
-        var params = prepareGetParams(hashtags);
-        $.getJSON('http://localhost:4567/?' + params, function (data) {
-            $('#tweets').empty();
-            data.forEach(function (t) {
-                $('#tweets').append('<a href="#" class="list-group-item">' + t.text + '</a>')
-            })
-        }).fail(function (error) {
-            console.log(error);
-        });
+        getTopTweets(hashtags, 10);
+        prevHashtags = hashtags;
+        prevNumberOfTweets = 10;
     });
+
+    getTopTweets(prevHashtags, prevNumberOfTweets);
+
+    setInterval(function () {
+        console.log('interval...');
+        getTopTweets(prevHashtags, prevNumberOfTweets);
+    }, 10 * 1000);
 });
+
+function clearTweets() {
+    $('#tweets').empty();
+}
+
+function enableProgressIndicator() {
+    clearTweets();
+    $('#tweets').text('update...');
+}
 
 function prepareGetParams(hashtags) {
     return hashtags
@@ -19,4 +32,17 @@ function prepareGetParams(hashtags) {
             return 'hashtags=' + h
         })
         .join('&')
+}
+
+function getTopTweets(hashtags, numberOfTweets) {
+    enableProgressIndicator();
+    var params = prepareGetParams(hashtags);
+    $.getJSON('http://localhost:4567/?' + params, function (data) {
+        clearTweets();
+        data.forEach(function (t) {
+            $('#tweets').append('<a href="#" class="list-group-item">' + t.text + '</a>')
+        })
+    }).fail(function (error) {
+        console.log(error);
+    });
 }
